aa9d46600bd88960a087500c2d56700e0bc44da5
12332188A <135287444+12332188A@users.noreply.github.com>
2023-09-10 09:06:40 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,31 @@
+from twitchio.ext import commands
+import keep_alive
+
+
+twitchBot = commands.Bot(
+    token="om5q311m1m8tavnd3zhgczebzur8zn", # ACCESS TOKEN, Get it at https://twitchtokengenerator.com
+    nick="kadiyena", # your bot's nick
+    prefix="!", # command prefix
+    client_id="gp762nuuoqcoxypju8c569th9wz7q5", # Get it at https://twitchtokengenerator.com
+    initial_channels=["you9100937a", "TwitchAccount2"], # The twitch channels you want the bot working at
+)
+
+
+@twitchBot.command(name="add") # call it at twitch chat with: !add
+async def add(ctx): # you can use any function name, doesn't matter
+  if len(ctx.message.content.split(" ")) == 3: # !add 1.5 2.5 (for example)
+    str1 = ctx.message.content.split(" ")[1]
+    str2 = ctx.message.content.split(" ")[2]
+    try:
+      num1 = float(str1)
+      num2 = float(str2)
+    except:
+      await ctx.send("please enter two numbers")
+      return
+    result_message = "%f + %f = %f" % (num1, num2, num1 + num2)
+    await ctx.send(result_message)
+
+
+if __name__ == '__main__':
+  
+  twitchBot.run()
